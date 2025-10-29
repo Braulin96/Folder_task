201efc06365607f393f7696996479b05fca68599
Braulin Pires <braulinpires@hotmail.com>
2025-10-29 14:06:32 +0000

    created the filter component to allow filter the files by name or date

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import FilterImage from "../../assets/icons/filter.svg";
+
+type Props = {
+  filters: string[];
+  selectedFilter?: string | null;
+  setSelectedFilter: (filter: string) => void;
+};
+
+const Filter = ({ filters, selectedFilter, setSelectedFilter }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenFilter = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelectedFilter = (filter: string) => {
+    setSelectedFilter(filter);
+    setIsOpen(false);
+  };
+
+  return (
+    <section className="relative">
+      <button
+        onClick={handleOpenFilter}
+        className=" bg-gray-600 rounded-[8px] flex px-[12px] py-[8px] gap-x-[4px] hover:bg-gray-500 transition-all duration-500">
+        <p className="text-[12px] text-gray-100 font-semibold">
+          {selectedFilter || "Select Filter"}
+        </p>
+        <img src={FilterImage} alt="filter" className="rotate-90 size-[20px]" />
+      </button>
+      {isOpen && (
+        <ul className="mt-[8px] bg-gray-500 rounded-[8px] shadow-lg p-[8px] flex flex-col gap-y-[4px] absolute z-10 w-full">
+          {filters?.map((filter, index) => (
+            <li
+              key={index}
+              onClick={() => handleSelectedFilter(filter)}
+              className="text-gray-200 text-[14px] hover:underline cursor-pointer font-semibold">
+              {filter}
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+};
+
+export default Filter;
--- src/components/FolderBlock/FolderBlock.tsx
+++ src/components/FolderBlock/FolderBlock.tsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 
 import Files from "../Files/Files";
 import Folder from "../Folder/Folder";
+import Filter from "../Filter/Filter";
 
 import { MOCK_FOLDER_DATA } from "../../data/folderData";
+import { FILTER_OPTIONS } from "../../data/filterOptions";
 
 const FolderBlock = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedFolderIndex, setSelectedFolderIndex] = useState<number | null>(
     null
   );
+
+  const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
 
   const handleOpenFolder = (index: number) => {
     setSelectedFolderIndex(index);
@@ -21,21 +25,30 @@ const FolderBlock = () => {
     setSelectedFolderIndex(null);
   };
 
+  console.log("selectedFilter:", selectedFilter);
+
   const folderData = MOCK_FOLDER_DATA;
 
   return (
     <div>
       {!isOpen ? (
-        <section
-          aria-label="folders section"
-          className="p-[30px] flex flex-wrap gap-[10px]">
-          {folderData.map((folder, index) => (
-            <Folder
-              key={index}
-              name={folder.name}
-              onClick={() => handleOpenFolder(index)}
+        <section aria-label="folders section" className="p-[30px] ">
+          <div aria-label="filter section" className="mb-[20px] w-fit">
+            <Filter
+              filters={FILTER_OPTIONS}
+              selectedFilter={selectedFilter}
+              setSelectedFilter={setSelectedFilter}
             />
-          ))}
+          </div>
+          <div className="flex flex-wrap gap-[10px]">
+            {folderData.map((folder, index) => (
+              <Folder
+                key={index}
+                name={folder.name}
+                onClick={() => handleOpenFolder(index)}
+              />
+            ))}
+          </div>
         </section>
       ) : (
         <section
