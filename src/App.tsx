b65a6f07375bad0f72e390b4648c409085610627
Braulin Pires <braulinpires@hotmail.com>
2025-10-29 11:12:28 +0000

    created file component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
+import Files from "./components/Files";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>file folder page</p>
+        <Files name="file1" fileType=".docx" dateAdded="12/02/12" />
       </header>
     </div>
   );
---
+++ src/components/Files.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  fileType?: string;
+  name?: string;
+  dateAdded?: string;
+  onClick?: () => void;
+};
+
+const Files = ({ fileType, name, dateAdded, onClick }: Props) => {
+  return (
+    <div>
+      <p>files here</p>
+      <div
+        onClick={onClick}
+        aria-label="file"
+        className="bg-gray-200 size-[120px] rounded-[12px] shadow-lg flex flex-col items-center justify-center">
+        <p className="text-gray-800 text-[16px]  mt-[40px]">{name}</p>
+        <div className="flex justify-between  w-full mt-auto mb-[8px] px-[4px]">
+          <p className="text-gray-600 text-[12px]">{dateAdded}</p>
+          <p className="text-gray-600 text-[12px]">{fileType}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Files;
