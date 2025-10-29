db339864a358ac81542b8bd62071814c0a32aca4
Braulin Pires <braulinpires@hotmail.com>
2025-10-29 12:35:39 +0000

    add some animations to the folder displayed

--- src/components/Folder/Folder.tsx
+++ src/components/Folder/Folder.tsx
@@ -10,13 +10,13 @@ const Folder = ({ name, onClick }: Props) => {
     <button
       onClick={onClick}
       aria-label="folder"
-      className="size-[120px] rounded-[12px] shadow-lg"
-      style={{
-        backgroundImage: `url(${FolderBackground})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}>
-      <p className="text-gray-800 text-[16px] mt-[20px]"> {name}</p>
+      className="size-[120px] rounded-[12px] shadow-lg bg-gray-600 hover:translate-y-[-6px] transition-all duration-[600] hover:bg-gray-500">
+      <img
+        src={FolderBackground}
+        alt="folder icon"
+        className="mx-auto  size-[90px]"
+      />
+      <p className="text-gray-100 text-[16px] mt-[2px] font-semibold">{name}</p>
     </button>
   );
 };
